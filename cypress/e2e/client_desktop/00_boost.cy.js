029026825821b98e0b1c106a986015aa3a19aeba
Janis Griezans <janisgriezans@hotmail.com>
2024-11-26 23:13:44 +0200

    Added test for language switching and improved beforeEach

--- cypress/e2e/client_desktop/00_boost.cy.js
+++ cypress/e2e/client_desktop/00_boost.cy.js
@@ -6,17 +6,32 @@ describe('BoostCasino Desktop Test Suite', () => {
     beforeEach(() => {
         urls.visitBoostHome();
         cy.handleCookieConsent();
+        lobby.confirmUserLoggedOut();
     });
     
     it('A (logged out) user should be able to find and open the game', () => {
         const gameName = 'Book of Dead';
 
-        lobby.confirmUserLoggedOut();
         lobby.searchInputVisible();
         lobby.searchForInput(gameName);
         lobby.checkGameVisible();
         lobby.checkListGameName(gameName);
         lobby.clickPlayButton();
         game.checkGameLoaded();
+    });
+
+    it('A (logged out) user should be able to switch language by the mean of the menu dropdown', () => {
+        const languages = [
+            { index: 0, langCode: 'FI', urlSuffix: '/fi', welcomeMessage: 'Tervetuloa Boost Casino', customerSupportMessage: 'Asiakaspalvelu' },
+            { index: 0, langCode: 'EE', urlSuffix: '/ee', welcomeMessage: 'Tere tulemast Boost Casino lehele', customerSupportMessage: 'Klienditugi' },
+            { index: 1, langCode: 'RU', urlSuffix: '/ru', welcomeMessage: 'Добро пожаловать в Boost Casino', customerSupportMessage: 'Служба поддержки' },
+        ];
+
+        languages.forEach(language => {
+            lobby.openLanguageDropdown();
+            lobby.checkLanguageFromDropdown(language.index, language.langCode);
+            lobby.clickOnLanguageFromDropdown(language.index);
+            lobby.verifyLanguageChange(language.urlSuffix, language.welcomeMessage, language.customerSupportMessage);
+        });
     });
 });
